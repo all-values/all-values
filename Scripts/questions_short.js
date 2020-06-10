263c4341936f6c014177b48ea6f9c9c67c0ea346
all-values <66677194+all-values@users.noreply.github.com>
2020-06-10 17:53:20 +0200

    Create questions_short.js

---
+++ Scripts/questions_short.js
@@ -0,0 +1,23 @@
+questions = [
+    {
+      "question":"Państwo powinno ponosić wszelkie koszty leczenia obywateli."
+      "effect":{
+          "econ": -10,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+      }
+    },
+    
+    {
+        "question":"W walce o zmiany nie możemy uciekać się do przemocy."
+      "effect":{
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": -10
+    }
+
+]
