eddcc1996e7d07cac24489907ac6644a3b8a98f1
all-values <66677194+all-values@users.noreply.github.com>
2020-11-12 12:18:09 +0100

    Update questions_short.js

--- Scripts/questions_short.js
+++ Scripts/questions_short.js
@@ -19,6 +19,39 @@ questions = [
           "scty": 0,
           "revo": -10
         }
+    },
+    
+     {
+        "question": "Prostytucja z moralnego punktu widzenia jest normalnym zawodem.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 10,
+          "revo": 0
+        }
+    },
+    
+    {
+        "question": "Kara śmierci powinna być możliwym wyrokiem za najcięższe przestępstwa.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 10,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+   {
+       "question": "Prawa obowiązujące w naszym państwie powinny być stanowione tylko przez jego obywateli.",
+      "effect": {
+          "econ": 0,
+          "dipl": -10,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+        }
     }
 
 ];
