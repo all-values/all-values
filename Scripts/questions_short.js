2f2fe11f385a67841e02a9e5861f52c8ddc05991
all-values <66677194+all-values@users.noreply.github.com>
2021-01-19 00:41:43 +0100

    Update questions_short.js

--- Scripts/questions_short.js
+++ Scripts/questions_short.js
@@ -17,7 +17,7 @@ questions = [
           "dipl": 0,
           "govt": 0,
           "scty": 0,
-          "revo": -10
+          "revo": 10
         }
     },
     
@@ -27,7 +27,7 @@ questions = [
           "econ": 0,
           "dipl": 0,
           "govt": 0,
-          "scty": 10,
+          "scty": -10,
           "revo": 0
         }
     },
@@ -52,6 +52,226 @@ questions = [
           "scty": 0,
           "revo": 0
         }
+    },
+    
+    {
+      "question": "Państwo powinno zagwarantować swobodę prowadzenia działalności gospodarczej.",
+      "effect": {
+          "econ": 10,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+      }
+    },
+    
+    {
+        "question": "Najskuteczniejsze zmiany dokonują się na przestrzeni wielu lat, wprowadzane powoli i stopniowo.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": -10
+        }
+    },
+    
+     {
+        "question": "Tradycja jest wartością sama w sobie.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 10,
+          "revo": 0
+        }
+    },
+    
+    {
+        "question": "Lista legalnych używek powinna zostać poszerzona.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": -10,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+   {
+       "question": "Państwa powinny dążyć do politycznej integracji.",
+      "effect": {
+          "econ": 0,
+          "dipl": -10,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+    {
+      "question": "Skala progresywna jest najuczciwszym sposobem opodatkowania dochodów obywateli.",
+      "effect": {
+          "econ": -10,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+      }
+    },
+    
+    {
+        "question": "Legislacja powinna opierać się na długoterminowych planach.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 10
+        }
+    },
+    
+     {
+        "question": "Religia jest najlepszym wyznacznikiem wartości moralnych.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 10,
+          "revo": 0
+        }
+    },
+    
+    {
+        "question": "Każdy obywatel powinien odbyć raz w życiu służbę wojskową.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 10,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+   {
+       "question": "Organizacje międzynarodowe są zagrożeniem dla suwerenności ich członków.",
+      "effect": {
+          "econ": 0,
+          "dipl": -10,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    {
+      "question": "Dochód z kapitału to uczciwy zarobek.",
+      "effect": {
+          "econ": 10,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+      }
+    },
+    
+    {
+        "question": "W obliczu cierpienia ludzi stopniowo wprowadzane zmiany nie są wystarczającym rozwiązaniem.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": -10
+        }
+    },
+    
+     {
+        "question": "Pary homoseksualne powinny mieć zagwarantowane te same prawa, co pary heteroseksualne.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": -10,
+          "revo": 0
+        }
+    },
+    
+    {
+        "question": "W sytuacji kryzysowej państwo ma prawo ograniczać wolność jednostki dla dobra ogółu.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 10,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+   {
+       "question": "Należy ograniczyć import zagranicznych produktów spożywczych do niezbędnego minimum.",
+      "effect": {
+          "econ": 0,
+          "dipl": 10,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+        }
     }
+
+{
+      "question": "Obywatele powinni mieć możliwość dopłacenia dodatkowej sumy pieniędzy aby otrzymać wyższą jakość usług publicznych, \n na przykład dostęp do lepszej jakości edukacji.",
+      "effect": {
+          "econ": 10,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+      }
+    },
+    
+    {
+        "question": "Niepokoje w społeczeństwie są naturalną i akceptowalną konsekwencją radykalnych zmian.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 0,
+          "revo": -10
+        }
+    },
+    
+     {
+        "question": "Bez wszechobecnej technologii ludziom żyło się lepiej.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 0,
+          "scty": 10,
+          "revo": 0
+        }
+    },
+    
+    {
+        "question": "Państwo powinno mieć możliwość inwigilacji podejrzanych osób wedle własnego uznania dla zapewnienia bezpieczeństwa.",
+      "effect": {
+          "econ": 0,
+          "dipl": 0,
+          "govt": 10,
+          "scty": 0,
+          "revo": 0
+        }
+    },
+    
+   {
+       "question": "Emigracja zarobkowa powinna byc nielegalna.",
+      "effect": {
+          "econ": 0,
+          "dipl": 10,
+          "govt": 0,
+          "scty": 0,
+          "revo": 0
+        }
+    }
+
 
 ];
