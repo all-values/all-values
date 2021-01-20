1cbade5d4926553902378740baa6889d8f950d3e
all-values <66677194+all-values@users.noreply.github.com>
2021-01-20 10:26:53 +0100

    Update questions_forbidden.js

--- Scripts/questions_forbidden.js
+++ Scripts/questions_forbidden.js
@@ -1,42 +1,42 @@
 questions = [
   {
-    "question" = "Catboye powinny być refundowane każdemu kto sobie ich pragnie.",
-    "effect" = -10
+    "question": "Catboye powinny być refundowane każdemu kto sobie ich pragnie.",
+    "effect": -10
   },
   {
-    "question" = "Każdy obywatel powinien móc zabić intruza na prywatnej posesji.",
-    "effect" = 10
+    "question": "Każdy obywatel powinien móc zabić intruza na prywatnej posesji.",
+    "effect": 10
   },
   {
-    "question" = "Catboye powinni mieć dedykowane miejsca w środkach transportu publicznego.",
-    "effect" = -10
+    "question": "Catboye powinni mieć dedykowane miejsca w środkach transportu publicznego.",
+    "effect": -10
   },
   {
-    "question" = "Demokracja w każdym państwie powinna być wzorowana na kolebce demokracji jaką są Stany Zjednoczone.",
-    "effect" = 10
+    "question": "Demokracja w każdym państwie powinna być wzorowana na kolebce demokracji jaką są Stany Zjednoczone.",
+    "effect": 10
   },
   {
-    "question" = "Karol Marks, Fryderyk Engels i Włodzimierz Lenin.",
-    "effect" = -10
+    "question": "Karol Marks, Fryderyk Engels i Włodzimierz Lenin.",
+    "effect": -10
   },
   {
-    "question" = "Syrop glukozowo-fruktozowy to najbardziej pożywny napój.",
-    "effect" = -10
+    "question": "Syrop glukozowo-fruktozowy to najbardziej pożywny napój.",
+    "effect": -10
   },
   {
-    "question" = "Twoja flaga nie jest lepszym manifestem osobowości niż moje uszka.",
-    "effect" = -10
+    "question": "Twoja flaga nie jest lepszym manifestem osobowości niż moje uszka.",
+    "effect": -10
   },
   {
-    "question" = "Każdy hymn narodowy najlepiej brzmi w wersji vocaloid",
-    "effect" = -10
+    "question": "Każdy hymn narodowy najlepiej brzmi w wersji vocaloid",
+    "effect": -10
   },
   {
-    "question" = "Furries nie są gejowe.",
-    "effect" = -10
+    "question": "Furries nie są gejowe.",
+    "effect": -10
   },
   {
-    "question" = "Jeśli politycy nie spełniają oczekiwań, należy ukraść im mównicę.",
-    "effect" = 10
+    "question": "Jeśli politycy nie spełniają oczekiwań, należy ukraść im mównicę.",
+    "effect": 10
   }
 ];
